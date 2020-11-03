5357c1fb23262f8c05a892d88b475344e1f897c0
Aankitraj <66714860+Aankitraj@users.noreply.github.com>
2020-11-03 15:05:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import stylee from './css/stylee';
+import {Link} from 'react-scroll';
+import Home from './home';
+import About from './About';
+import Shopping from './Shopping';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div style={stylee.page}>
+      {/* logo*/}
+      <section style={{zIndex: '5', top: 0 , height: '10 0px'}}>
+        <img src="https://pbs.twimg.com/profile_images/1012477112928780289/U1z2JjFX_400x400.jpg" style={{height: '90px', width: '100px'}} />
+      </section>
+      {/* navigation */}
+      <div style={stylee.head}>
+        <div style={{width: '40%'}}></div>
+
+          <nav style={{
+            color: 'white ',
+            height: '40px',
+            width: '60%',
+            display: 'flex',
+            justifyContent: 'space-around',
+            padding: '5px'
+          }}>
+          <Link to="About" style={stylee.bttn} >About</Link>
+          <Link to="Services" style={stylee.bttn} >Services</Link>
+          <Link to="/" style={stylee.bttn} >Contact</Link>
+          <Link to="/" style={stylee.bttn} >Updates</Link>
+          </nav>
+
+      </div>
+      <Home />
+
+      {/* About */}
+      <div id="About" >
+        <br />
+        <br />
+        <About />
+      </div>
+
+      {/* Shopping */}
+      <div id="Services" >
+        <br />
+        <br />
+        <Shopping />
+      </div>
+
+
+    <div>
+      {/*  */}
     </div>
+    {/* footer */}
+    <div style={stylee.footer}>
+        <div>
+          posts
+          <ul>
+            <li>programms</li>
+            <li>programms</li>
+            <li>programms</li>
+          </ul>
+        </div>
+
+        <div>
+          categories
+          <ul>
+            <li>programms</li>
+            <li>programms</li>
+            <li>programms</li>
+          </ul>
+        </div>
+
+        <div>
+          ids
+          <ul>
+            <li>programms</li>
+            <li>programms</li>
+            <li>programms</li>
+          </ul>
+        </div>
+    </div>
+  </div>
   );
 }
+
 
 export default App;
